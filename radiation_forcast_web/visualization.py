64fd20b800e89d0e0985c150457434c71da71f80
EthanSeok <champ9162@naver.com>
2024-07-19 04:20:12 +0900

    ASOS 광 시각화 추가

--- radiation_forcast_web/visualization.py
+++ radiation_forcast_web/visualization.py
@@ -105,6 +105,12 @@ def visualize_data(start_date, end_date, stn_ids, reg_cd):
     tomorrow_df['timestamp'] = pd.to_datetime(tomorrow_df['fcstDate'].astype(str) + ' ' + tomorrow_df['fcstTime'],
                                               format='%Y-%m-%d %H:%M')
 
+    today_df = today_df.sort_values(by='timestamp').reset_index(drop=True)
+    tomorrow_df = tomorrow_df.sort_values(by='timestamp').reset_index(drop=True)
+
+    today_df = today_df.drop_duplicates(subset=['timestamp'])
+    tomorrow_df = tomorrow_df.drop_duplicates(subset=['timestamp'])
+
 
     # 선택된 날짜 범위 내의 데이터 필터링
     filtered_today_df = today_df[
@@ -115,7 +121,7 @@ def visualize_data(start_date, end_date, stn_ids, reg_cd):
     # 선택 옵션 추가
     options = st.multiselect(
         "출력할 그래프 선택",
-        ["예측 그래프", "예측 산점도 그래프", "ASOS 비교 그래프"]
+        ["예측 그래프", "예측 산점도 그래프", "ASOS 비교 그래프", 'ASOS 비교 산점도 그래프']
     )
 
     # 그래프 표시 버튼
@@ -329,24 +335,208 @@ def visualize_data(start_date, end_date, stn_ids, reg_cd):
             # Plotly 차트 표시
             st.plotly_chart(fig_wind_scatter)
 
-        # if "ASOS 비교 그래프" in options:
-        #     fig_rad = go.Figure()
-        #     fig_rad.add_trace(go.Scatter(x=ASOS_today['tm'],
-        #                                  y=ASOS_today['일사(MJ/m2)'],
-        #                                  mode='lines',
-        #                                  name='Today',
-        #                                  line=dict(color='#FFD700')))
-        #     fig_rad.add_trace(
-        #         go.Scatter(x=ASOS_today['tm'],
-        #                    y=ASOS_today['예측광량'],
-        #                    mode='lines',
-        #                    name='Tomorrow',
-        #                    line=dict(color='#FFA500', dash='dash')))
-        #     fig_rad.update_layout(title='예측광량', xaxis_title='Timestamp', yaxis_title='Radiation')
-        #     st.plotly_chart(fig_rad)
+        if "ASOS 비교 그래프" in options:
+            # 예측광량 데이터 프레임 병합
+            combined_radiation_df = pd.concat([
+                filtered_today_df[['timestamp', '예측광량']].assign(day='Today'),
+                filtered_tomorrow_df[['timestamp', '예측광량']].assign(day='Tomorrow')
+            ])
+
+            combined_radiation_df['예측광량'] = combined_radiation_df['예측광량'] * 0.0036
+
+            # 예측광량 그래프
+            fig_rad = go.Figure()
+            fig_rad.add_trace(go.Scatter(x=asos_df['tm'],
+                                        y=asos_df['일사(MJ/m2)'],
+                                        mode='lines',
+                                        name='ASOS',
+                                        line=dict(color='#FF4000')))
+
+            fig_rad.add_trace(go.Scatter(x=combined_radiation_df[combined_radiation_df['day'] == 'Today']['timestamp'],
+                                         y=combined_radiation_df[combined_radiation_df['day'] == 'Today']['예측광량'],
+                                         mode='lines',
+                                         name='Today',
+                                         line=dict(color='#FFD700')))
+            fig_rad.add_trace(
+                go.Scatter(x=combined_radiation_df[combined_radiation_df['day'] == 'Tomorrow']['timestamp'],
+                           y=combined_radiation_df[combined_radiation_df['day'] == 'Tomorrow']['예측광량'],
+                           mode='lines',
+                           name='Tomorrow',
+                           line=dict(color='#FFA500', dash='dash')))
+
+            fig_rad.update_layout(title='일사(MJ/m2)', xaxis_title='Timestamp', yaxis_title='Radiation')
+            st.plotly_chart(fig_rad)
 
 
+            # 예측온도 데이터 프레임 병합
+            combined_temp_df = pd.concat([
+                filtered_today_df[['timestamp', '예측온도']].assign(day='Today'),
+                filtered_tomorrow_df[['timestamp', '예측온도']].assign(day='Tomorrow')
+            ])
 
+            # 예측온도 그래프
+            fig_temp = go.Figure()
+            fig_temp.add_trace(go.Scatter(x=asos_df['tm'],
+                                          y=asos_df['온도'],
+                                          mode='lines',
+                                          name='Today',
+                                          line=dict(color='#FFBF00')))
+            fig_temp.add_trace(go.Scatter(x=combined_temp_df[combined_temp_df['day'] == 'Today']['timestamp'],
+                                          y=combined_temp_df[combined_temp_df['day'] == 'Today']['예측온도'],
+                                          mode='lines',
+                                          name='Today',
+                                          line=dict(color='#FF6347')))
+            fig_temp.add_trace(go.Scatter(x=combined_temp_df[combined_temp_df['day'] == 'Tomorrow']['timestamp'],
+                                          y=combined_temp_df[combined_temp_df['day'] == 'Tomorrow']['예측온도'],
+                                          mode='lines',
+                                          name='Tomorrow',
+                                          line=dict(color='#FF4500', dash='dash')))
+            fig_temp.update_layout(title='예측온도', xaxis_title='Timestamp', yaxis_title='Temperature')
+            st.plotly_chart(fig_temp)
+
+
+            # 예측풍속 데이터 프레임 병합
+            combined_wind_df = pd.concat([
+                filtered_today_df[['timestamp', '예측풍속']].assign(day='Today'),
+                filtered_tomorrow_df[['timestamp', '예측풍속']].assign(day='Tomorrow')
+            ])
+
+            # 예측풍속 그래프
+            fig_wind = go.Figure()
+            fig_wind.add_trace(go.Scatter(x=asos_df['tm'],
+                                          y=asos_df['풍속'],
+                                          mode='lines',
+                                          name='Today',
+                                          line=dict(color='#A4A4A4')))
+            fig_wind.add_trace(go.Scatter(x=combined_wind_df[combined_wind_df['day'] == 'Today']['timestamp'],
+                                          y=combined_wind_df[combined_wind_df['day'] == 'Today']['예측풍속'],
+                                          mode='lines',
+                                          name='Today',
+                                          line=dict(color='#1E90FF')))
+            fig_wind.add_trace(go.Scatter(x=combined_wind_df[combined_wind_df['day'] == 'Tomorrow']['timestamp'],
+                                          y=combined_wind_df[combined_wind_df['day'] == 'Tomorrow']['예측풍속'],
+                                          mode='lines',
+                                          name='Tomorrow',
+                                          line=dict(color='#4169E1', dash='dash')))
+            fig_wind.update_layout(title='예측풍속', xaxis_title='Timestamp', yaxis_title='Wind Speed')
+            st.plotly_chart(fig_wind)
+
+        if "ASOS 비교 산점도 그래프" in options:
+            # 예측 광량 산점도
+            filtered_today_df['timestamp'] = pd.to_datetime(filtered_today_df['timestamp'])
+            filtered_tomorrow_df['timestamp'] = pd.to_datetime(filtered_tomorrow_df['timestamp'])
+            asos_df['tm'] = pd.to_datetime(asos_df['tm'])
+
+            merge_radiation_df = pd.merge(
+                filtered_today_df[['timestamp', '예측광량']].assign(day='Today'),
+                filtered_tomorrow_df[['timestamp', '예측광량']].assign(day='Tomorrow'),
+                how='inner', on=['timestamp'], suffixes=('_Today', '_Tomorrow')
+            )
+
+            merge_radiation_df = pd.merge(merge_radiation_df, asos_df[['tm', '일사(MJ/m2)']], left_on='timestamp',
+                                          right_on='tm', how='inner')
+            merge_radiation_df['예측광량_Today'] = merge_radiation_df['예측광량_Today'] * 0.0036
+            merge_radiation_df['예측광량_Tomorrow'] = merge_radiation_df['예측광량_Tomorrow'] * 0.0036
+
+
+            today_data = merge_radiation_df['예측광량_Today']
+            asos = merge_radiation_df['일사(MJ/m2)']
+
+            # 선형 회귀 모델 적합
+            X = sm.add_constant(asos)  # 독립 변수에 상수 추가
+            model = sm.OLS(today_data, X).fit()
+
+            # R² 값과 RMSE 계산
+            r_squared = model.rsquared
+            rmse = np.sqrt(model.mse_resid)
+
+            # 산점도 플롯 생성
+            fig_rad_scatter = go.Figure()
+            fig_rad_scatter.add_trace(go.Scatter(
+                x=asos,
+                y=today_data,
+                mode='markers',
+                name='Data'
+            ))
+
+            # 추세선 추가
+            fig_rad_scatter.add_trace(go.Scatter(
+                x=asos,
+                y=model.predict(X),
+                mode='lines',
+                line=dict(color='red'),
+                name=f'Trendline (R²={r_squared:.2f}, RMSE={rmse:.2f})'
+            ))
+
+            # 레이아웃 업데이트 (1:1 비율로 설정)
+            fig_rad_scatter.update_layout(
+                title='Today 와 ASOS 광량 비교',
+                xaxis_title='ASOS 광량',
+                yaxis_title='Today 예측광량',
+                xaxis=dict(scaleanchor="y", scaleratio=1),
+                yaxis=dict(scaleanchor="x", scaleratio=1)
+            )
+
+            # Plotly 차트 표시
+            st.plotly_chart(fig_rad_scatter)
+
+            # 예측 광량 산점도
+            filtered_today_df['timestamp'] = pd.to_datetime(filtered_today_df['timestamp'])
+            filtered_tomorrow_df['timestamp'] = pd.to_datetime(filtered_tomorrow_df['timestamp'])
+            asos_df['tm'] = pd.to_datetime(asos_df['tm'])
+
+            merge_radiation_df = pd.merge(
+                filtered_today_df[['timestamp', '예측광량']].assign(day='Today'),
+                filtered_tomorrow_df[['timestamp', '예측광량']].assign(day='Tomorrow'),
+                how='inner', on=['timestamp'], suffixes=('_Today', '_Tomorrow')
+            )
+
+            merge_radiation_df = pd.merge(merge_radiation_df, asos_df[['tm', '일사(MJ/m2)']], left_on='timestamp',
+                                          right_on='tm', how='inner')
+            merge_radiation_df['예측광량_Today'] = merge_radiation_df['예측광량_Today'] * 0.0036
+            merge_radiation_df['예측광량_Tomorrow'] = merge_radiation_df['예측광량_Tomorrow'] * 0.0036
+
+
+            Tomorrow_data = merge_radiation_df['예측광량_Tomorrow']
+            asos = merge_radiation_df['일사(MJ/m2)']
+
+            # 선형 회귀 모델 적합
+            X = sm.add_constant(asos)  # 독립 변수에 상수 추가
+            model = sm.OLS(today_data, X).fit()
+
+            # R² 값과 RMSE 계산
+            r_squared = model.rsquared
+            rmse = np.sqrt(model.mse_resid)
+
+            # 산점도 플롯 생성
+            fig_rad_scatter = go.Figure()
+            fig_rad_scatter.add_trace(go.Scatter(
+                x=asos,
+                y=Tomorrow_data,
+                mode='markers',
+                name='Data'
+            ))
+
+            # 추세선 추가
+            fig_rad_scatter.add_trace(go.Scatter(
+                x=asos,
+                y=model.predict(X),
+                mode='lines',
+                line=dict(color='red'),
+                name=f'Trendline (R²={r_squared:.2f}, RMSE={rmse:.2f})'
+            ))
+
+            # 레이아웃 업데이트 (1:1 비율로 설정)
+            fig_rad_scatter.update_layout(
+                title='Tomorrow 와 ASOS 광량 비교',
+                xaxis_title='ASOS 광량',
+                yaxis_title='Tomorrow 예측광량',
+                xaxis=dict(scaleanchor="y", scaleratio=1),
+                yaxis=dict(scaleanchor="x", scaleratio=1)
+            )
+
+            # Plotly 차트 표시
+            st.plotly_chart(fig_rad_scatter)
 
 # if __name__ == "__main__":
 #     visualize_data()
