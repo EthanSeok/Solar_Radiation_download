c9b90cfe283fcfed19ce19711e1fd61edc3a30ee
EthanSeok <champ9162@naver.com>
2024-07-19 04:20:03 +0900

    필요없는 함수 제거

--- radiation_forcast_web/asos_download.py
+++ radiation_forcast_web/asos_download.py
@@ -6,7 +6,6 @@ import ssl
 from requests.adapters import HTTPAdapter
 from datetime import datetime
 
-
 class SSLAdapter(HTTPAdapter):
     def init_poolmanager(self, *args, **kwargs):
         context = ssl.create_default_context()
@@ -68,31 +67,6 @@ def fetch_weather_data(start_date, end_date, stn_ids, service_key):
     else:
         return None
 
-def save_data(df, region_code, cache_dir):
-    df['year_month'] = pd.to_datetime(df['tm']).dt.to_period('M').astype(str).str.replace('-', '_')
-
-    for year_month, group in df.groupby('year_month'):
-        filename = os.path.join(cache_dir, f"ASOS_{region_code}_{year_month}.csv")
-        group.to_csv(filename, index=False, encoding='utf-8-sig')
-        # st.write(f"Saved cache: {filename}")
-
-def cache_to_final(region_code, cache_dir, output_dir):
-    current_year_month = datetime.now().strftime('%Y_%m')
-    cached_files = [os.path.join(cache_dir, f) for f in os.listdir(cache_dir) if f.startswith(f"ASOS_{region_code}") and not f"_{current_year_month}.csv" in f]
-    all_data = []
-
-    for file in cached_files:
-        df = pd.read_csv(file)
-        all_data.append(df)
-
-    if all_data:
-        final_df = pd.concat(all_data, ignore_index=True)
-        # final_filename = os.path.join(output_dir, f"weather_data_{region_code}.csv")
-        # final_df.to_csv(final_filename, index=False, encoding='utf-8-sig')
-        # st.write(f"Saved final data: {final_filename}")
-    else:
-        st.write("No cached data to concatenate.")
-
 def process_asos_data(asos):
     asos = asos.copy()
     asos['일시'] = pd.to_datetime(asos['tm'], format='%Y-%m-%d %H:%M')
@@ -102,3 +76,5 @@ def process_asos_data(asos):
     asos = asos[['stnNm', '날짜', '시간', 'icsr', 'ta', 'ws', 'tm']]
     asos.rename(columns={'icsr': '일사(MJ/m2)', 'stnNm':'지점', 'ta': '온도', 'ws': '풍속'}, inplace=True)
     return asos
+
+
